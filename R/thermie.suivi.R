1d096821c57dd095a5f463b79a41a46a4fd590ac
jbfagotfede39 <jbfagot.fede39@gmail.com>
2015-12-07 11:59:35 +0100

    Function correction

--- R/thermie.suivi.R
+++ R/thermie.suivi.R
@@ -2,19 +2,24 @@
 #'
 #' Cette fonction permet d'extraire des données de suivi de terrain
 #' 
-#' @param Type Type de base de données
+#' @param Type Type de donnée de suivi
 #' @keywords 
-#' @import aquatools dplyr
+#' @import aquatools DBI dplyr
 #' @export
 #' @examples
-#' thermie.suivi("Station", "ORA2-7")
+#' thermie.suivi("CG39", Type = "MO")
+#' thermie.suivi("JB-Stéphane", Type = "Opérateurs")
+#' thermie.suivi("DRO14-2", Type = "Station")
+#' thermie.suivi("2015-10-23", Type = "Date")
+#' thermie.suivi("9759803", Type = "Sonde")
 
 ##### TODO LIST #####
 # 
 #####################
 
-thermie.suivi <- function(x, 
-                           Type = c("MO", "Opérateurs", "Station", "Date", "Sonde"), recherche = "ORA2-7")
+thermie.suivi <- function(x = "ORA2-7", 
+                          Type = c("MO", "Opérateurs", "Station", "Date", "Sonde")
+                          )
 {
 
   ## Évaluation des choix
@@ -26,49 +31,57 @@ thermie.suivi <- function(x,
   ## Chargement des données ##
   SuiviTerrain <- dbReadTable(db, "SuiviTerrain")
   
-  ## Recherche en tant que telle
+  ## x en tant que telle
   if(Type == "MO") 
+    Vue <-
   SuiviTerrain %>% 
-    filter(MO == recherche) %>% 
-    #filter(Opérateurs == recherche) %>% 
-    #filter(CodeRDT == recherche) %>% 
-    #filter(Date == recherche) %>% 
-    #filter(Capteur == recherche) %>% 
+    filter(MO == x) %>% 
+    #filter(Opérateurs == x) %>% 
+    #filter(CodeRDT == x) %>% 
+    #filter(Date == x) %>% 
+    #filter(Capteur == x) %>% 
     arrange(desc(Date))
   
-  if(Type == "Opérateurs") 
+  if(Type == "Opérateurs")
+    Vue <-
     SuiviTerrain %>% 
-    #filter(MO == recherche) %>% 
-    filter(Opérateurs == recherche) %>% 
-    #filter(CodeRDT == recherche) %>% 
-    #filter(Date == recherche) %>% 
-    #filter(Capteur == recherche) %>% 
+    #filter(MO == x) %>% 
+    filter(Opérateurs == x) %>% 
+    #filter(CodeRDT == x) %>% 
+    #filter(Date == x) %>% 
+    #filter(Capteur == x) %>% 
     arrange(desc(Date))
   
   if(Type == "Station") 
+    Vue <-
     SuiviTerrain %>% 
-    #filter(MO == recherche) %>% 
-    #filter(Opérateurs == recherche) %>% 
-    filter(CodeRDT == recherche) %>% 
-    #filter(Date == recherche) %>% 
-    #filter(Capteur == recherche) %>% 
+    #filter(MO == x) %>% 
+    #filter(Opérateurs == x) %>% 
+    filter(CodeRDT == x) %>% 
+    #filter(Date == x) %>% 
+    #filter(Capteur == x) %>% 
     arrange(desc(Date))
   
   if(Type == "Date") 
+    Vue <-
     SuiviTerrain %>% 
-    #filter(MO == recherche) %>% 
-    #filter(Opérateurs == recherche) %>% 
-    #filter(CodeRDT == recherche) %>% 
-    filter(Date == recherche) %>% 
-    #filter(Capteur == recherche) %>% 
+    #filter(MO == x) %>% 
+    #filter(Opérateurs == x) %>% 
+    #filter(CodeRDT == x) %>% 
+    filter(Date == x) %>% 
+    #filter(Capteur == x) %>% 
     arrange(desc(Date))
   
   if(Type == "Sonde") 
+    Vue <-
     SuiviTerrain %>% 
-    #filter(MO == recherche) %>% 
-    #filter(Opérateurs == recherche) %>% 
-    #filter(CodeRDT == recherche) %>% 
-    #filter(Date == recherche) %>% 
-    filter(Capteur == recherche) %>% 
+    #filter(MO == x) %>% 
+    #filter(Opérateurs == x) %>% 
+    #filter(CodeRDT == x) %>% 
+    #filter(Date == x) %>% 
+    filter(Capteur == x) %>% 
     arrange(desc(Date))
+  
+  ## Affichage des résultats ##
+  return(Vue)
 }
