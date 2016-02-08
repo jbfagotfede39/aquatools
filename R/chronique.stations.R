64bce1c4ef914202d5cf1206072d71718eb35dc6
jbfagotfede39 <jbfagot.fede39@gmail.com>
2016-02-08 11:08:09 +0100

    Creation of chronique.stations

---
+++ R/chronique.stations.R
@@ -0,0 +1,104 @@
+#' Listage des stations de chroniques
+#'
+#' Cette fonction permet de lister les stations de la BDD Chroniques
+#' 
+#' @param Nom Nom recherché
+#' @param Type Type de donnée de suivi
+#' @keywords stations
+#' @import dplyr DBI RSQLite
+#' @export
+#' @examples
+#' chronique.stations("CD39","MO")
+#' chronique.stations("Suran","Milieu")
+#' chronique.stations("Ain","Bassin")
+#' chronique.stations("Lons-le-Saunier","Commune")
+#' chronique.stations("39","Département")
+
+##### TODO LIST #####
+
+#####################
+
+stations.ecosysteme <- function(x = "CD39", 
+                                Type = c("MO", "Milieu", "Bassin", "Sous-bassin", "Commune", "Département")
+)
+{
+  
+  ## Évaluation des choix
+  Type <- match.arg(Type)
+  
+  ## Connexion à la BDD ##
+  db <- BDD.ouverture(Type = "Chroniques")
+  
+  ## Chargement des données ##
+  Stations <- dbReadTable(db, "Stations")
+  
+  ## x en tant que telle
+  if(Type == "MO") 
+    Vue <-
+    Stations %>% 
+    filter(MO == x) %>% 
+    #filter(Milieu == x) %>% 
+    #filter(Bassin == x) %>% 
+    #filter(Sous-bassi == x) %>% 
+    #filter(Commune == x) %>% 
+    #filter(Departement == x) %>% 
+    arrange(CodeRDT)
+  
+  if(Type == "Milieu") 
+    Vue <-
+    Stations %>% 
+    #filter(MO == x) %>% 
+    filter(Milieu == x) %>% 
+    #filter(Bassin == x) %>% 
+    #filter(Sous-bassi == x) %>% 
+    #filter(Commune == x) %>% 
+    #filter(Departement == x) %>% 
+    arrange(CodeRDT)
+  
+  if(Type == "Bassin") 
+    Vue <-
+    Stations %>% 
+    #filter(MO == x) %>% 
+    #filter(Milieu == x) %>% 
+    filter(Bassin == x) %>% 
+    #filter(Sous-bassi == x) %>% 
+    #filter(Commune == x) %>% 
+    #filter(Departement == x) %>% 
+    arrange(CodeRDT)
+  
+  if(Type == "Sous-bassin") 
+    Vue <-
+    Stations %>% 
+    #filter(MO == x) %>% 
+    #filter(Milieu == x) %>% 
+    #filter(Bassin == x) %>% 
+    filter(Sous-bassi == x) %>% 
+    #filter(Commune == x) %>% 
+    #filter(Departement == x) %>% 
+    arrange(CodeRDT)
+  
+  if(Type == "Commune") 
+    Vue <-
+    Stations %>% 
+    #filter(MO == x) %>% 
+    #filter(Milieu == x) %>% 
+    #filter(Bassin == x) %>% 
+    #filter(Sous-bassi == x) %>% 
+    filter(Commune == x) %>% 
+    #filter(Departement == x) %>% 
+    arrange(CodeRDT)
+  
+  if(Type == "Département") 
+    Vue <-
+    Stations %>% 
+    #filter(MO == x) %>% 
+    #filter(Milieu == x) %>% 
+    #filter(Bassin == x) %>% 
+    #filter(Sous-bassi == x) %>% 
+    #filter(Commune == x) %>% 
+    filter(Departement == x) %>% 
+    arrange(CodeRDT)
+  
+  ## Affichage des résultats ##
+  return(Vue)
+}
---
+++ R/chronique.validation.R
@@ -0,0 +1,34 @@
+#' Validation des chroniques
+#'
+#' Cette fonction permet d'obtenir des éléments afin de valider les chroniques
+#' 
+#' @param data Chronique à valider
+#' @keywords 
+#' @import dplyr DBI RSQLite
+#' @export
+#' @examples
+#' chronique.validation(data)
+
+##### TODO LIST #####
+# 
+#####################
+
+BDD.ouverture <- function(data)
+{
+  data <-
+    data %>% 
+    mutate(DateFine = ymd_hms(paste(Date, Heure, sep = "_"))) %>% 
+    arrange(DateFine) %>% 
+    mutate(Difference = DateFine - lag(DateFine)) # calcule l'écart de temps entre une valeur et la valeur précédente
+  
+  data %>% 
+    filter(Difference != 1, Validation != "Rejeté") 
+  
+  verif <-
+    data %>% 
+    complete(Date, Heure) %>% 
+    filter(is.na(Niveau) == T)
+  if(length(levels(as.factor(verif$Date))) > 2) print("Vérification nécessaire") else print("Seulement 2 journées incomplètes")
+  levels(as.factor(verif$Date)) # faire la différence entre ce vecteur et les min et max de ensemble
+  
+} # Fin de la fonction
