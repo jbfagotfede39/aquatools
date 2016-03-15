ee8aa9e1690103ccd1648ec187458b2d78dc900f
jbfagotfede39 <jbfagot.fede39@gmail.com>
2016-03-15 17:00:12 +0100

    SIG.exportSHP correction and BDD.SIG creation

---
+++ R/BDD.SIG.R
@@ -0,0 +1,83 @@
+#' Export BDD vers SIG des stations
+#'
+#' Cette fonction permet d'exporter automatiquement les stations des différentes bases de données vers les couches SIG correspondantes
+#' 
+#' @param data Chronique à valider
+#' @keywords data
+#' @import dplyr DBI lubridate
+#' @export
+#' @examples
+#' BDD.SIG()
+
+###### À faire #####
+# 
+####################
+
+BDD.SIG <- function()
+{
+
+
+  ###### Chroniques ######
+  db <- BDD.ouverture("Chroniques")
+  Stations <- dbReadTable(db, "Stations")
+  Stations <- Stations %>% filter(!is.na(XL93))
+  
+  if(file.exists("/Users/imac27/hubiC/Données/Chroniques/BDD_Chroniques_FD39.sqlite") == T){
+  file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques.dbf", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Chroniques/archives/", format(now(), format="%Y-%m-%d"),"_Stations_chroniques.dbf"), overwrite = T)
+  file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques.prj", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Chroniques/archives/", format(now(), format="%Y-%m-%d"),"_Stations_chroniques.prj"), overwrite = T)
+  file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques.shp", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Chroniques/archives/", format(now(), format="%Y-%m-%d"),"_Stations_chroniques.shp"), overwrite = T)
+  file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques.shx", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Chroniques/archives/", format(now(), format="%Y-%m-%d"),"_Stations_chroniques.shx"), overwrite = T)
+  SIG.exportSHP(Stations, Stations$XL93, Stations$YL93, "/Users/imac27/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques")
+  }
+  
+  if(file.exists("/Users/jean-baptistefagot_FD39/hubiC/Données/Chroniques/BDD_Chroniques_FD39.sqlite") == T){
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques.dbf", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Chroniques/archives/", format(now(), format="%Y-%m-%d"),"_Stations_chroniques.dbf"), overwrite = T)
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques.prj", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Chroniques/archives/", format(now(), format="%Y-%m-%d"),"_Stations_chroniques.prj"), overwrite = T)
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques.shp", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Chroniques/archives/", format(now(), format="%Y-%m-%d"),"_Stations_chroniques.shp"), overwrite = T)
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques.shx", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Chroniques/archives/", format(now(), format="%Y-%m-%d"),"_Stations_chroniques.shx"), overwrite = T)
+    SIG.exportSHP(Stations, Stations$XL93, Stations$YL93, "/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Chroniques/Stations_chroniques")
+  }
+  
+  ###### Poissons ######
+  db <- BDD.ouverture("Poissons")
+  Stations <- dbReadTable(db, "Stations")
+  Stations <- Stations %>% filter(TypeLambert == 'L93')
+  
+    if(file.exists("/Users/imac27/hubiC/Données/Poissons/Base poisson FD/MaxiFish_V3/multifish - fede39.sqlite") == T){
+    file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons.dbf", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Poissons/archives/", format(now(), format="%Y-%m-%d"),"_Stations_poissons.dbf"), overwrite = T)
+    file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons.prj", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Poissons/archives/", format(now(), format="%Y-%m-%d"),"_Stations_poissons.prj"), overwrite = T)
+    file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons.shp", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Poissons/archives/", format(now(), format="%Y-%m-%d"),"_Stations_poissons.shp"), overwrite = T)
+    file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons.shx", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Poissons/archives/", format(now(), format="%Y-%m-%d"),"_Stations_poissons.shx"), overwrite = T)
+    SIG.exportSHP(Stations, Stations$XLambert, Stations$YLambert, "/Users/imac27/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons")
+  }
+  
+  if(file.exists("/Users/jean-baptistefagot_FD39/hubiC/Données/poissons/BDD_poissons_FD39.sqlite") == T){
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons.dbf", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Poissons/archives/", format(now(), format="%Y-%m-%d"),"_Stations_poissons.dbf"), overwrite = T)
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons.prj", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Poissons/archives/", format(now(), format="%Y-%m-%d"),"_Stations_poissons.prj"), overwrite = T)
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons.shp", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Poissons/archives/", format(now(), format="%Y-%m-%d"),"_Stations_poissons.shp"), overwrite = T)
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons.shx", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Poissons/archives/", format(now(), format="%Y-%m-%d"),"_Stations_poissons.shx"), overwrite = T)
+    SIG.exportSHP(Stations, Stations$XLambert, Stations$YLambert, "/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Poissons/Stations_poissons")
+  }
+  
+  ###### Physico-chimie ######
+  db <- BDD.ouverture("Physico-chimie")
+  Stations <- dbReadTable(db, "Operations")
+  Stations <- Stations %>% filter(!is.na(XL93))
+  
+  if(file.exists("/Users/imac27/hubiC/Données/Physico-chimie/BDD_Physico-chimie_FD39.sqlite") == T){
+    file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC.dbf", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Physico-chimie/archives/", format(now(), format="%Y-%m-%d"),"_Stations_PC.dbf"), overwrite = T)
+    file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC.prj", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Physico-chimie/archives/", format(now(), format="%Y-%m-%d"),"_Stations_PC.prj"), overwrite = T)
+    file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC.shp", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Physico-chimie/archives/", format(now(), format="%Y-%m-%d"),"_Stations_PC.shp"), overwrite = T)
+    file.copy("/Users/imac27/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC.shx", paste0("/Users/imac27/hubiC/SIG/Données/Réseaux/Physico-chimie/archives/", format(now(), format="%Y-%m-%d"),"_Stations_PC.shx"), overwrite = T)
+    SIG.exportSHP(Stations, Stations$XL93, Stations$YL93, "/Users/imac27/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC")
+  }
+  
+  if(file.exists("/Users/jean-baptistefagot_FD39/hubiC/Données/Physico-chimie/BDD_Physico-chimie_FD39.sqlite") == T){
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC.dbf", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Physico-chimie/archives/", format(now(), format="%Y-%m-%d"),"_Stations_PC.dbf"), overwrite = T)
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC.prj", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Physico-chimie/archives/", format(now(), format="%Y-%m-%d"),"_Stations_PC.prj"), overwrite = T)
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC.shp", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Physico-chimie/archives/", format(now(), format="%Y-%m-%d"),"_Stations_PC.shp"), overwrite = T)
+    file.copy("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC.shx", paste0("/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Physico-chimie/archives/", format(now(), format="%Y-%m-%d"),"_Stations_PC.shx"), overwrite = T)
+    SIG.exportSHP(Stations, Stations$XL93, Stations$YL93, "/Users/jean-baptistefagot_FD39/hubiC/SIG/Données/Réseaux/Physico-chimie/Stations_PC")
+  }
+  
+} # Fin de la fonction
--- R/SIG.exportSHP.R
+++ R/SIG.exportSHP.R
@@ -10,7 +10,7 @@
 #' @import DBI dplyr rgdal RSQLite sp
 #' @export
 #' @examples
-#' SIG.exportSHP(Stations, XLambert, YLambert, "Export_stations")
+#' SIG.exportSHP(Stations, "Stations$XL93", "Stations$YL93", "Export_stations")
 
 ##### TODO LIST #####
 # Bug à l'export des stations de thermie depuis la BDD :
@@ -18,17 +18,35 @@
 # alors que ça fonctionne si on passe directement : coordinates(Stations) <- ~XL93+YL93
 # ce qui semble poser pb est la transfo de nom de XL93 en XLambert par la fonction, car il n'y a ensuite pas de XLambert dans le dataframe et la fct coordinates refuse de fonctionner
 
+# SIG.exportSHP(Stations, Stations$XL93, Stations$YL93, "Export_stations")
+
 #####################
 
 SIG.exportSHP <- function(
   Data = Stations,
-  XLambert = XLambert,
-  YLambert = YLambert,
+  a = Stations$XL93,
+  b = Stations$YL93,
   nomfichier = "Export_stations")
 {
+
+  Data <-
+    Data %>% 
+    #filter(!is.na(XL93)) %>% 
+    mutate(XL = a) %>% 
+    mutate(YL = b)
   
+    #Data <-
+    #Stations %>% 
+    #filter(!is.na(XL93)) %>% 
+    #mutate(XL = XL93) %>% 
+    #mutate(YL = YL93)
+  
+    #Data <-
+    #Data %>% 
+    #filter(!is.na(XL93))  
+
   ##### Écriture d'une couche SIG #####
-coordinates(Data) <- ~XLambert+YLambert # Permet de créer un SpatialPointsDataFrame à partir d'un dataframe Data contenant les coordonnées dans les colonnes XLambert et YLambert
+coordinates(Data) <- ~XL+YL # Permet de créer un SpatialPointsDataFrame à partir d'un dataframe Data contenant les coordonnées dans les colonnes XLambert et YLambert
 proj4string(Data) <- CRS("+init=epsg:2154") # Définition de la projection, ici pour Lambert 93
 # proj4string(Data) <- CRS("+init=IGNF:LAMBE") # Pour LambertIIétendu
 setCPLConfigOption("SHAPE_ENCODING", "UTF-8") # Afin de définir l'encodage en UT8
