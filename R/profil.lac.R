7f3fc2cc520ba3f1254ba402817523542f6d88e7
jbfagotfede39 <jbfagot.fede39@gmail.com>
2016-09-15 15:00:59 +0200

    Update function with chemicals database

--- R/profil.lac.R
+++ R/profil.lac.R
@@ -5,7 +5,7 @@
 #' @param O2pourc
 #' @param Cond 
 #' @param ph
-#' @keywords cats
+#' @param temp
 #' @export
 #' @import ggplot2
 #' @examples
@@ -13,6 +13,7 @@
 #' profil.lac(PC,param="O2pourc")
 #' profil.lac(PC,param="Cond")
 #' profil.lac(PC,param="ph")
+#' #' profil.lac(PC,param="temp")
 
 # Il faudra affiner la présentation
 # Il faudra rajouter un paramètre save=T, en sous option if dans chaque bloc
@@ -20,43 +21,68 @@
 profil.lac<-function(PC,param="O2mg"){
   
   if(param=="O2mg"){
-  ggplot(PC, aes(Profondeurcor)) +
-  geom_point(aes(y = O2mg, colour = "O2 (mg/L)"),size = 5) +
-  geom_point(aes(y = T, colour = "Température (°C)"),size = 5) +
-  geom_line(aes(y = O2mg, colour = "O2 (mg/L)")) +
-  geom_line(aes(y = T, colour = "Température (°C)")) +
-  labs(color = "Légende",x = "Profondeur (m)", y = "",title=paste(levels(PC$Lac)[1],levels(PC$Date)[1],sep=" le ")) + # Pour changer le titre de la légende
-  coord_flip() # pour inverser l'affichage des X et des Y
+  ggO2 <- ggplot(subset(PC, (ParametreSANDRE == 1311)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
+  ggO2 <- ggO2 + geom_point(stat="identity") 
+  ggO2 <- ggO2 + geom_line(linetype="dashed")
+  ggO2 <- ggO2 + scale_color_manual(
+    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"),
+    name="Date")
+  ggO2 <- ggO2 + labs(x = "Profondeur (m)", y = expression(Oxygène~dissous~(mg~O[2]/L))) # Pour changer le titre
+  ggO2 <- ggO2 + coord_flip() # pour inverser l'affichage des X et des Y
+  ggO2 <- ggO2 + theme_bw()
+  ggO2
   }
 
   else if(param=="O2pourc"){
-  ggplot(PC, aes(Profondeurcor)) +
-    geom_point(aes(y = O2pourc, colour = "O2 (% saturation)"),size = 5) +
-    geom_point(aes(y = T, colour = "Température (°C)"),size = 5) +
-    geom_line(aes(y = O2pourc, colour = "O2 (% saturation)")) +
-    geom_line(aes(y = T, colour = "Température (°C)")) +
-    labs(color = "Légende",x = "Profondeur (m)", y = "",title=paste(levels(PC$Lac)[1],levels(PC$Date)[1],sep=" le ")) + # Pour changer le titre de la légende
-    coord_flip() # pour inverser l'affichage des X et des Y
+  gg <- ggplot(subset(PC, (ParametreSANDRE == 1312)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
+  gg <- gg + geom_point(stat="identity")
+  gg <- gg + geom_line(linetype="dashed")
+  gg <- gg + scale_color_manual(
+    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"),
+    name="Date")
+  gg <- gg + labs(x = "Profondeur (m)", y = expression(Oxygène~dissous~("%"~saturation~O[2]/L))) # Pour changer le titre
+  gg <- gg + coord_flip() # pour inverser l'affichage des X et des Y
+  gg <- gg + theme_bw()
+  gg
   }
   
   else if(param=="Cond"){
-    ggplot(PC, aes(Profondeurcor)) +
-      geom_point(aes(y = Cond, colour = "Conductivité (uS)"),size = 5) +
-      geom_point(aes(y = T, colour = "Température (°C)"),size = 5) +
-      geom_line(aes(y = Cond, colour = "Conductivité (uS)")) +
-      geom_line(aes(y = T, colour = "Température (°C)")) +
-      labs(color = "Légende",x = "Profondeur (m)", y = "",title=paste(levels(PC$Lac)[1],levels(PC$Date)[1],sep=" le ")) + # Pour changer le titre de la légende
-      coord_flip() # pour inverser l'affichage des X et des Y
+  gg <- ggplot(subset(PC, (ParametreSANDRE == 1303)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
+  gg <- gg + geom_point(stat="identity")
+  gg <- gg + geom_line(linetype="dashed")
+  gg <- gg + scale_color_manual(
+    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"),
+    name="Date")
+  gg <- gg + labs(x = "Profondeur (m)", y = expression(Conductivité~corrigée~à~25~degree*C~(paste(mu,S)))) # Pour changer le titre
+  gg <- gg + coord_flip() # pour inverser l'affichage des X et des Y
+  gg <- gg + theme_bw()
+  gg
   }
   
   else if(param=="ph"){
-    ggplot(PC, aes(Profondeurcor)) +
-      geom_point(aes(y = ph, colour = "pH"),size = 5) +
-      geom_point(aes(y = T, colour = "Température (°C)"),size = 5) +
-      geom_line(aes(y = ph, colour = "pH")) +
-      geom_line(aes(y = T, colour = "Température (°C)")) +
-      labs(color = "Légende",x = "Profondeur (m)", y = "",title=paste(levels(PC$Lac)[1],levels(PC$Date)[1],sep=" le ")) + # Pour changer le titre de la légende
-      coord_flip() # pour inverser l'affichage des X et des Y
+  gg <- ggplot(subset(PC, (ParametreSANDRE == 1302)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
+  gg <- gg + geom_point(stat="identity")
+  gg <- gg + geom_line(linetype="dashed")
+  gg <- gg + scale_color_manual(
+    values = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2","#D55E00"),
+    name="Date")
+  gg <- gg + labs(x = "Profondeur (m)", y = expression(pH)) # Pour changer le titre
+  gg <- gg + coord_flip() # pour inverser l'affichage des X et des Y
+  gg <- gg + theme_bw()
+  gg
+  }
+  
+  else if(param=="temp"){
+  gg <- ggplot(subset(PC, (ParametreSANDRE == 1301)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
+  gg <- gg + geom_point(stat="identity") 
+  gg <- gg + geom_line(linetype="dashed")
+  gg <- gg + scale_color_manual(
+    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"),
+    name="Date")
+  gg <- gg + labs(x = "Profondeur (m)", y = expression(Température~(degree*C))) # Pour changer le titre
+  gg <- gg + coord_flip() # pour inverser l'affichage des X et des Y
+  gg <- gg + theme_bw()
+  gg
   }
 
 } # Fin de la fonction
