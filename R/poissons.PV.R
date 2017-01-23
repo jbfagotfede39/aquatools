36b47d9988c672981136b2416ad480ae2e27806e
jbfagotfede39 <jbfagot.fede39@gmail.com>
2017-01-23 15:13:27 +0100

    - poissons.PV() : création

---
+++ R/poissons.PV.R
@@ -0,0 +1,59 @@
+#' Exportation des PV
+#'
+#' Cette fonction permet d'exporter les données de PV de gestion piscicole
+#' 
+#' @param ecosysteme Code de l'écosystème
+#' @keywords poissons
+#' @import dplyr DBI RSQLite lubridate
+#' @export
+#' @examples
+#' poissons.PV()
+#' poissons.PV("ROU")
+
+##### TODO LIST #####
+
+#####################
+
+#library(aquatools);library(dplyr);library(lubridate);library(RSQLite);ecosysteme="ROU"
+
+poissons.PV <- function(
+  ecosysteme="")
+{
+  
+  ## Ouverture de la BDD ##
+  db <- BDD.ouverture(Type = "Poissons")
+  #dbListTables(db)
+  
+  ## Récupération des données ##
+  #Captures <- dbReadTable(db, "Captures")
+  #Operations <- dbReadTable(db, "Operations") %>% select(Codeoperation, AvisExpertCourt, AvisExpert)
+  #IPR <- dbReadTable(db, "IPRs")
+  #Inventaires <- dbReadTable(db, "Inventaires")
+  #Stations <- dbReadTable(db, "Stations")
+  Ecosystemes <- dbReadTable(db, "Ecosystemes")
+  pv_lots <- dbReadTable(db, "pv_lots")
+  pv_pvs <- dbReadTable(db, "pv_pvs")
+  
+  ## Synthèse des données ##
+  pv_lots <- left_join(pv_lots, Ecosystemes, by = c("codeecosysteme" = "Codeecosysteme")) #
+  pv_lots <- left_join(pv_lots, pv_pvs, by = "numero_pv") #
+  
+  ## Format de dates ##
+  pv_lots$date_pv <- ymd_hms(pv_lots$date_pv)
+  pv_lots$date_pv <- format(pv_lots$date_pv, "%Y-%m-%d")
+  
+  ## Simplification ##
+  pv_lots <- 
+    pv_lots %>%
+    filter(codeRDT == ecosysteme) %>% 
+    rename(Date = date_pv) %>% 
+    arrange(desc(Date))
+  
+  ##### Sorties des résultats traités au format Excel #####
+  
+  #write.xlsx(x = IPR, file = paste0(station, "_", date, "_IPR.xlsx"),
+  #           sheetName = paste0(station, " ", date), row.names = F)
+  
+  return(pv_lots)
+  
+} # Fin de la fonction
