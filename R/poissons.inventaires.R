4486a85123d28075a006e174bab95ea001ac288c
jbfagotfede39 <jbfagot.fede39@gmail.com>
2017-02-09 11:24:54 +0100

    - Création poissons.inventaires() - Modification poissons.operations()

--- R/poissons.inventaires.R
+++ R/poissons.inventaires.R
@@ -1,62 +1,61 @@
-#' Listing des opérations pour une station
+#' Listing des inventaires pour une station
 #'
-#' Cette fonction permet de lister les différentes opérations pour une station donnée
+#' Cette fonction permet de lister les différents inventaires pour une station donnée ou l'ensemble de la BDD
 #' 
 #' @param station Code de la station
 #' @keywords poissons
 #' @import dplyr RSQLite DBI lubridate
 #' @export
 #' @examples
-#' poissons.operations()
-#' poissons.operations("SOR10-2")
+#' poissons.inventaires()
+#' poissons.inventaires("SOR10-2")
 
 ##### TODO LIST #####
 # 
 #####################
 
-poissons.operations <- function(
+poissons.inventaires <- function(
   station="")
 {
-
+  
   ## Ouverture de la BDD ##
   ## Connexion à la BDD
   db <- BDD.ouverture(Type = "Poissons")
   #dbListTables(db)
   ## Récupération des données ##
   #Captures <- dbReadTable(db, "Captures")
-  Operations <- dbReadTable(db, "Operations")
+  #Operations <- dbReadTable(db, "Operations")
   Inventaires <- dbReadTable(db, "Inventaires")
   Stations <- dbReadTable(db, "Stations")
   
   ## Synthèse des données ##
   Inventaires <- merge(Inventaires, Stations, by = c("CodeStation"))
-  Operations <- merge(Operations, Inventaires, by = c("CodeInventaire"))
   
   ## Format de dates ##
-  Operations$DateDebut.x <- ymd_hms(Operations$DateDebut.x)
-  Operations$DateDebut.x <- format(Operations$DateDebut.x, "%Y-%m-%d")
+  Inventaires$DateDebut <- ymd_hms(Inventaires$DateDebut)
+  Inventaires$DateDebut <- format(Inventaires$DateDebut, "%Y-%m-%d")
   
   ## Extraction des stations ##
   # Test si le nom existe bien, sinon message d'erreur et arrêt de la fonction #
   
-  if(dim(Operations %>% filter(Nom == station))[1] == 0 & nchar(station) != 0)
-    warning("Attention : nom de station absent des opérations de la base de données")
+  if(dim(Inventaires %>% filter(Nom == station))[1] == 0 & nchar(station) != 0)
+    warning("Attention : nom de station absent des inventaires de la base de données")
   
   ## Simplification ##
   # Travail sur une seule station
   if(nchar(station) != 0){
-    Operations <- 
-      Operations %>%
-    select(Nom, DateDebut.x) %>% 
-    filter(Nom == station) %>% 
-    dplyr::rename(Stations = Nom, Dates = DateDebut.x)}
+    Inventaires <- 
+      Inventaires %>%
+      select(Nom, DateDebut) %>% 
+      filter(Nom == station) %>% 
+      dplyr::rename(Stations = Nom, Dates = DateDebut)}
   
   # Travail sur l'ensemble des opérations
   if(nchar(station) == 0){
-    Operations <- 
-      Operations %>%
-      select(Nom, DateDebut.x) %>% 
+    Inventaires <- 
+      Inventaires %>%
+      select(Nom, DateDebut) %>% 
       #filter(Nom == station) %>% 
-      dplyr::rename(Stations = Nom, Dates = DateDebut.x)}
-    
+      dplyr::rename(Stations = Nom, Dates = DateDebut)}
+  
 } # Fin de la fonction
