d2409f3a8d3ad2c12bd16a90b194adfcff1d45af
jbfagotfede39 <jbfagot.fede39@gmail.com>
2017-02-22 11:42:11 +0100

    - Complément de poissons.captures avec export du code capture et fusion avec la fusion poissons.captures.BDD() (deprecated)

--- R/poissons.captures.R
+++ R/poissons.captures.R
@@ -2,14 +2,20 @@
 #'
 #' Récupère les données de captures de poissons d'une opération
 #' @keywords donnees
+#' @param station CodeRDT de la station
+#' @param date Date de l'opération
+#' @param codeCapture Affichage du CodeCapture - \code{FALSE} (par défault) 
 #' @import dplyr RSQLite DBI lubridate
 #' @export
 #' @examples
+#' poissons.captures()
 #' poissons.captures("SOR10-2", "2015-05-19")
+#' poissons.captures("SOR10-2", "2015-05-19",codeCapture=T)
 
 poissons.captures <- function(  
-  station="SOR10-2",
-  date="2015-05-19")
+  station="",
+  date="",
+  codeCapture = FALSE)
 {
   
 #library("RSQLite");library("dplyr")
@@ -33,15 +39,36 @@ Captures$DateDebut <- ymd_hms(Captures$DateDebut)
 Captures$DateDebut <- format(Captures$DateDebut, "%Y-%m-%d")
 
 ##### Filtrage #####
+if(nchar(station) != 0 & nchar(date) != 0){
 Captures <-
   Captures %>% 
   filter(Nom == station, DateDebut == date) %>%
-  arrange(NumeroDePassage, CodeEspece)
+  arrange(NumeroDePassage, CodeEspece)}
+
+if(nchar(station) != 0 & nchar(date) == 0){
+  Captures <-
+    Captures %>% 
+    filter(Nom == station) %>%
+    arrange(NumeroDePassage, CodeEspece)}
+
+if(nchar(station) == 0 & nchar(date) != 0){
+  Captures <-
+    Captures %>% 
+    filter(DateDebut == date) %>%
+    arrange(NumeroDePassage, CodeEspece)}
+
+# On garde captures identiques si les deux paramètres sont vides pour avoir toute la base de données
 
 ##### Simplification #####
+if(codeCapture == FALSE){ # Si on ne veut pas le CodeCapture
 Captures <- 
   Captures %>%
-  select(Nom, DateDebut, NumeroDePassage, CodeEspece, TailleMinimum, TailleMaximum, Nombre, Poids)
+   select(Nom, DateDebut, NumeroDePassage, CodeEspece, TailleMinimum, TailleMaximum, Nombre, Poids)}
+
+if(codeCapture == TRUE){ # Si on veut le CodeCapture
+  Captures <- 
+    Captures %>%
+    select(CodeCapture, Nom, DateDebut, NumeroDePassage, CodeEspece, TailleMinimum, TailleMaximum, Nombre, Poids)}
 
 ##### Calcul d'une colonne taille (avec la taille moyenne pour les lots) #####
 Captures <- 
