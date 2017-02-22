8ed2082b171fdebf280576dbc0761f8a585a4ba8
jbfagotfede39 <jbfagot.fede39@gmail.com>
2017-02-22 08:58:40 +0100

    - Création de formatage.ecosysteme() - Homogénéisation des noms de formatage.abreviation() + formatage.annee.biologique + formatage.date

---
+++ R/formatage.ecosysteme.R
@@ -0,0 +1,78 @@
+#' Mise en acronymes des écosystèmes
+#'
+#' Cette fonction permet de mettre en acronymes des noms d'écosystèmes
+#' @export
+#' @import tidyverse stringr
+#' @examples
+#' formatage.ecosysteme(data$Station)
+#' PC$CodeRDT <- formatage.ecosysteme(PC$CodeRDT)
+
+##### TODO LIST #####
+# 
+#####################
+
+formatage.ecosysteme <- function(
+  acronymes=data$Station
+  )
+  {
+  
+  #### Mise en minuscule ####
+  # Afin de s'affranchir des problèmes de casse
+  acronymes <-
+    acronymes %>% 
+    str_to_lower()
+
+  #### Détection et remplacement ####
+acronymes[str_detect(acronymes, "abbaye")] <- "ABB"
+acronymes[str_detect(acronymes, "antre")] <- "ANT"
+acronymes[str_detect(acronymes, "assencière")] <- "ASS"
+acronymes[str_detect(acronymes, "bellefontaine")] <- "BEL"
+acronymes[str_detect(acronymes, "bez")] <- "BEZ"
+acronymes[str_detect(acronymes, "bonlieu")] <- "BON"
+acronymes[str_detect(acronymes, "brenet")] <- "BRN"
+acronymes[str_detect(acronymes, "chalain")] <- "CHN"
+acronymes[str_detect(acronymes, "chanon")] <- "CAO"
+acronymes[str_detect(acronymes, "clairvaux pet")] <- "PCL"
+acronymes[str_detect(acronymes, "clairvaux grd")] <- "GCL"
+acronymes[str_detect(acronymes, "chambly")] <- "CHY"
+acronymes[str_detect(acronymes, "dame")] <- "DAM"
+acronymes[str_detect(acronymes, "embouteilleux")] <- "EMB"
+acronymes[str_detect(acronymes, "etival")] <- "GET"
+acronymes[str_detect(acronymes, "petit etival")] <- "PET"
+acronymes[str_detect(acronymes, "fauge")] <- "FAU"
+acronymes[str_detect(acronymes, "plasne")] <- "FDP"
+acronymes[str_detect(acronymes, "fioget")] <- "FIO"
+acronymes[str_detect(acronymes, "ilay")] <- "ILA"
+acronymes[str_detect(acronymes, "lamoura")] <- "LAM"
+acronymes[str_detect(acronymes, "lautrey")] <- "LAU"
+acronymes[str_detect(acronymes, "petit maclu")] <- "PMA"
+acronymes[str_detect(acronymes, "grand maclu")] <- "GMA"
+acronymes[str_detect(acronymes, "mortes")] <- "MOR"
+acronymes[str_detect(acronymes, "narlay")] <- "NAR"
+acronymes[str_detect(acronymes, "onoz")] <- "ONO"
+acronymes[str_detect(acronymes, "penne")] <- "PEN"
+acronymes[str_detect(acronymes, "ratay")] <- "RAT"
+acronymes[str_detect(acronymes, "truites")] <- "RGT"
+acronymes[str_detect(acronymes, "rousses")] <- "ROU"
+acronymes[str_detect(acronymes, "rosay")] <- "ROS"
+acronymes[str_detect(acronymes, "val")] <- "LVA"
+acronymes[str_detect(acronymes, "vernois")] <- "VER"
+acronymes[str_detect(acronymes, "viremont")] <- "VIR"
+acronymes[str_detect(acronymes, "viry")] <- "VRY"
+acronymes[str_detect(acronymes, "blye")] <- "BLY"
+acronymes[str_detect(acronymes, "bolozon")] <- "CIZ"
+acronymes[str_detect(acronymes, "coiselet")] <- "COI"
+acronymes[str_detect(acronymes, "cuttura")] <- "CUT"
+acronymes[str_detect(acronymes, "etables")] <- "ETA"
+acronymes[str_detect(acronymes, "lavancia")] <- "LAV"
+acronymes[str_detect(acronymes, "ravilloles")] <- "RAV"
+acronymes[str_detect(acronymes, "mortier")] <- "MOT"
+acronymes[str_detect(acronymes, "vouglans")] <- "VOU"
+
+  #### Test de complétude ####
+  if(all(grepl("^[[:upper:]]+$", acronymes)) != T) warning(paste0("Certain(s) cas non traité(s) : ",unique(acronymes[!grepl("^[[:upper:]]+$", acronymes)])))
+
+  #### Retour du tableau complet ####
+  return(acronymes)
+  
+} # Fin de la fonction
