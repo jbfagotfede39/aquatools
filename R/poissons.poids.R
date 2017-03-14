8bf4180d48e0dcba631c3282f2306e14602ed5ee
jbfagotfede39 <jbfagot.fede39@gmail.com>
2017-03-14 16:18:13 +0100

    - poissons.poids() : rajout de library(FSA)

--- R/poissons.poids.R
+++ R/poissons.poids.R
@@ -2,7 +2,7 @@
 #'
 #' Calcule, à partir d'une jeu de données de captures, les poids attendus pour des tailles données
 #' @keywords donnees
-#' #@import FSA
+#' @import FSA
 #' @import car 
 #' @import magrittr 
 #' @import dplyr
