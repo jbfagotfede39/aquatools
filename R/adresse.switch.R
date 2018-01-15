a4f937742a3c41e134a530f376c6e34538ea0b3c
jbfagotfede39 <jbfagot.fede39@gmail.com>
2018-01-15 12:55:52 +0100

    - adresse.switch() : ajout de la machine d'Adrien

--- R/adresse.switch.R
+++ R/adresse.switch.R
@@ -6,6 +6,7 @@
 #' @export
 #' @examples
 #' adresse.switch("NAS-JB/Études/2014_Les Rousses/Résultats/Poissons/ResultatsRoussestotal-28janvier2015.xlsx")
+#' adresse.switch("NAS-DATA/Poissons/PDPG/Stations PDPG_V1.xlsx")
 #' fish <- read_excel(adresse.switch("NAS-JB/Études/2014_Les Rousses/Résultats/Poissons/ResultatsRoussestotal-28janvier2015.xlsx"), sheet = 2)
 
 ##### TODO LIST #####
@@ -22,8 +23,9 @@ adresse.switch <- function(
   if(nchar(url) == 0) url = readline(prompt = "Saisir une adresse de fichier : ")
   
   #### Transformation du format ####
-  if(file.exists(paste0("/Users/imac27/",url))) url <- paste0("/Users/imac27/",url)
-  if(file.exists(paste0("/Volumes/Fixe-FD39/",url))) url <- paste0("/Volumes/Fixe-FD39/",url)
+  if(file.exists(paste0("/Users/imac27/",url))) url <- paste0("/Users/imac27/",url) # Machine fixe JB
+  if(file.exists(paste0("/Volumes/Fixe-FD39/",url))) url <- paste0("/Volumes/Fixe-FD39/",url) # Machine portable JB
+  if(file.exists(paste0("/Users/adrienlavigne/",url))) url <- paste0("/Users/adrienlavigne/",url) # Machine Adrien
   
   #### Retour de l'adresse correcte ####
   return(url)
