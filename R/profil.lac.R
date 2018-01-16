f320785b88029be0bb6c2d4134c09da83106a9f2
jbfagotfede39 <jbfagot.fede39@gmail.com>
2018-01-16 17:25:14 +0100

    - profil.lac() : ajout d'un détecteur pour les cas avec plus de 8 chroniques

--- R/profil.lac.R
+++ R/profil.lac.R
@@ -24,10 +24,9 @@ profil.lac<-function(PC,param="O2mg"){
   ggO2 <- ggplot(subset(PC, (ParametreSANDRE == 1311)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
   ggO2 <- ggO2 + geom_point(stat="identity") 
   ggO2 <- ggO2 + geom_line(linetype="dashed")
-  ggO2 <- ggO2 + scale_color_manual(
-    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"),
-    name="Date")
-  ggO2 <- ggO2 + labs(x = "Profondeur (m)", y = expression(Oxygene~dissous~(mg~O[2]/L))) # Pour changer le titre
+  if(subset(PC, (ParametreSANDRE == 1311)) %>% distinct(Date) %>% nrow() <= 8) gg <- gg + scale_color_manual(
+    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"))
+  ggO2 <- ggO2 + labs(x = "Profondeur (m)", y = expression(Oxygene~dissous~(mg~O[2]/L)), color = "Date") # Pour changer le titre
   ggO2 <- ggO2 + coord_flip() # pour inverser l'affichage des X et des Y
   ggO2 <- ggO2 + theme_bw()
   ggO2
@@ -37,10 +36,9 @@ profil.lac<-function(PC,param="O2mg"){
   gg <- ggplot(subset(PC, (ParametreSANDRE == 1312)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
   gg <- gg + geom_point(stat="identity")
   gg <- gg + geom_line(linetype="dashed")
-  gg <- gg + scale_color_manual(
-    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"),
-    name="Date")
-  gg <- gg + labs(x = "Profondeur (m)", y = expression(Oxygene~dissous~("%"~saturation~O[2]/L))) # Pour changer le titre
+  if(subset(PC, (ParametreSANDRE == 1312)) %>% distinct(Date) %>% nrow() <= 8) gg <- gg + scale_color_manual(
+    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"))
+  gg <- gg + labs(x = "Profondeur (m)", y = expression(Oxygene~dissous~("%"~saturation~O[2]/L)), color = "Date") # Pour changer le titre
   gg <- gg + coord_flip() # pour inverser l'affichage des X et des Y
   gg <- gg + theme_bw()
   gg
@@ -50,10 +48,9 @@ profil.lac<-function(PC,param="O2mg"){
   gg <- ggplot(subset(PC, (ParametreSANDRE == 1303)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
   gg <- gg + geom_point(stat="identity")
   gg <- gg + geom_line(linetype="dashed")
-  gg <- gg + scale_color_manual(
-    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"),
-    name="Date")
-  gg <- gg + labs(x = "Profondeur (m)", y = expression(Conductivite~corrigee~à~25~degree*C~(paste(mu,S)))) # Pour changer le titre
+  if(subset(PC, (ParametreSANDRE == 1303)) %>% distinct(Date) %>% nrow() <= 8) gg <- gg + scale_color_manual(
+    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"))
+  gg <- gg + labs(x = "Profondeur (m)", y = expression(Conductivite~corrigee~à~25~degree*C~(paste(mu,S))), color = "Date") # Pour changer le titre
   gg <- gg + coord_flip() # pour inverser l'affichage des X et des Y
   gg <- gg + theme_bw()
   gg
@@ -63,10 +60,9 @@ profil.lac<-function(PC,param="O2mg"){
   gg <- ggplot(subset(PC, (ParametreSANDRE == 1302)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
   gg <- gg + geom_point(stat="identity")
   gg <- gg + geom_line(linetype="dashed")
-  gg <- gg + scale_color_manual(
-    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"),
-    name="Date")
-  gg <- gg + labs(x = "Profondeur (m)", y = expression(pH~(unite~pH))) # Pour changer le titre
+  if(subset(PC, (ParametreSANDRE == 1302)) %>% distinct(Date) %>% nrow() <= 8) gg <- gg + scale_color_manual(
+    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"))
+  gg <- gg + labs(x = "Profondeur (m)", y = expression(pH~(unite~pH)), color = "Date") # Pour changer le titre
   gg <- gg + coord_flip() # pour inverser l'affichage des X et des Y
   gg <- gg + theme_bw()
   gg
@@ -76,10 +72,9 @@ profil.lac<-function(PC,param="O2mg"){
   gg <- ggplot(subset(PC, (ParametreSANDRE == 1301)), aes(-1*Profondeur, Valeur, colour=as.character(Date)))
   gg <- gg + geom_point(stat="identity") 
   gg <- gg + geom_line(linetype="dashed")
-  gg <- gg + scale_color_manual(
-    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"),
-    name="Date")
-  gg <- gg + labs(x = "Profondeur (m)", y = expression(Temperature~(degree*C))) # Pour changer le titre
+  if(subset(PC, (ParametreSANDRE == 1301)) %>% distinct(Date) %>% nrow() <= 8) gg <- gg + scale_color_manual(
+    values = c("#D55E00", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7", "#999999", "#0072B2"))
+  gg <- gg + labs(x = "Profondeur (m)", y = expression(Temperature~(degree*C)), color = "Date") # Pour changer le titre
   gg <- gg + coord_flip() # pour inverser l'affichage des X et des Y
   gg <- gg + theme_bw()
   gg
