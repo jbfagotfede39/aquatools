65cb772bcf713b4c455cbd2d325d029532323f0c
jbfagotfede39 <jbfagot.fede39@gmail.com>
2018-08-13 12:02:24 +0200

    - poissons.inventaires() : mise à jour des noms de champs

--- R/poissons.inventaires.R
+++ R/poissons.inventaires.R
@@ -36,12 +36,12 @@ poissons.inventaires <- function(
   Inventaires <- merge(Inventaires, Stations, by = c("codestation"))
   
   ## Format de dates ##
-  Inventaires$DateDebut <- ymd(Inventaires$DateDebut)
+  Inventaires$datedebut <- ymd(Inventaires$datedebut)
   
   ## Extraction des stations ##
   # Test si le nom existe bien, sinon message d'erreur et arrêt de la fonction #
   
-  if(dim(Inventaires %>% filter(Nom == station))[1] == 0 & nchar(station) != 0)
+  if(dim(Inventaires %>% filter(nom == station))[1] == 0 & nchar(station) != 0)
     warning("Attention : nom de station absent des inventaires de la base de données")
   
   ## Simplification ##
@@ -49,31 +49,31 @@ poissons.inventaires <- function(
   if(nchar(station) != 0 & all == F){
     Inventaires <- 
       Inventaires %>%
-      select(Nom, DateDebut) %>% 
-      filter(Nom == station) %>% 
-      dplyr::rename(Stations = Nom, Dates = DateDebut)}
+      select(nom, datedebut) %>% 
+      filter(nom == station) %>% 
+      dplyr::rename(Stations = nom, Dates = datedebut)}
   
   if(nchar(station) != 0 & all == T){
     Inventaires <- 
       Inventaires %>%
-      #select(Nom, DateDebut) %>% 
-      filter(Nom == station) %>% 
-      dplyr::rename(Stations = Nom, Dates = DateDebut)}
+      #select(nom, datedebut) %>% 
+      filter(nom == station) %>% 
+      dplyr::rename(Stations = nom, Dates = datedebut)}
   
   # Travail sur l'ensemble des opérations
   if(nchar(station) == 0 & all == F){
     Inventaires <- 
       Inventaires %>%
-      select(Nom, DateDebut) %>% 
-      #filter(Nom == station) %>% 
-      dplyr::rename(Stations = Nom, Dates = DateDebut)}
+      select(nom, datedebut) %>% 
+      #filter(nom == station) %>% 
+      dplyr::rename(Stations = nom, Dates = datedebut)}
   
   if(nchar(station) == 0 & all == T){
     Inventaires <- 
       Inventaires %>%
-      #select(Nom, DateDebut) %>% 
-      #filter(Nom == station) %>% 
-      dplyr::rename(Stations = Nom, Dates = DateDebut)}
+      #select(nom, datedebut) %>% 
+      #filter(nom == station) %>% 
+      dplyr::rename(Stations = nom, Dates = datedebut)}
   
   return(Inventaires)
   
