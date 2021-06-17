0363f0ff10c0baeecb3811c082045fc416a762e2
jbfagotfede39 <jbfagot.fede39@gmail.com>
2021-06-17 14:59:42 +0200

    - PC.saturation : Renommage d'une variable - chronique.cle : ajout de la modalité HST - chronique.ouverture : refactoring d'un test pour supprimer des warnings et remise en place de l'évaluation des choix et ajout du scénario miniDOTregroupe et correction d'un traitement pour miniDOTindividuel

--- R/PC.saturationO2.R
+++ R/PC.saturationO2.R
@@ -2,17 +2,14 @@
 #'
 #' Cette fonction permet de calculer des saturations en oxygène à partir des chroniques de mesures de concentration et de température de l'eau
 #' @name PC.saturationO2
-#' @param data Jeu de données (contenant une colonne Thermie et une colonne Oxygenation)
+#' @param data Jeu de données (contenant une colonne Thermie et une colonne Concentration)
 #' @keywords Physico-chimie
 #' @import tidyverse
 #' @export
 #' @examples
 #' PC.saturationO2(data)
+#' dataaimporter %>% PC.saturationO2()
 #' # Calcul à partir de https://projects.ncsu.edu/cals/course/zo419/oxygen.html
-
-##### TODO LIST #####
-
-#####################
 
 PC.saturationO2 <- function(data)
 {
@@ -20,8 +17,8 @@ PC.saturationO2 <- function(data)
   data <- 
     data %>% 
     mutate(O2sat = 14.59 - 0.3955*Thermie + 0.0072*Thermie^2 - 0.0000619*Thermie^3) %>% 
-    mutate(O2perc = Oxygenation/O2sat*100) %>% 
-    select(-O2sat)
+    mutate(Saturation = Concentration/O2sat*100) %>% 
+    dplyr::select(-O2sat)
   
   return(data)
   
